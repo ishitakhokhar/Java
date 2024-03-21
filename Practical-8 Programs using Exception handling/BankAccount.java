a22bd37149c2999eb3eac26e6892551609f16941
ishitakhokhar <144801034+ishitakhokhar@users.noreply.github.com>
2024-03-21 21:09:47 +0530

    Add files via upload

---
+++ Practical-8 Programs using Exception handling/BankAccount.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+class InsufficientFundException extends Exception {
+    InsufficientFundException(String s) {
+        super(s);
+    }
+}
+
+class Account {
+    double balance;
+
+    Account(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposit : " + amount);
+        System.out.println("Current balance : " + balance);
+    }
+
+    void withdraw(double amount) throws InsufficientFundException {
+        if (amount > balance) {
+            throw new InsufficientFundException("Sorry, you cannot withdraw more than the current balance");
+        }
+        balance -= amount;
+        System.out.println("Withdraw : " + amount);
+        System.out.println("Current balance : " + balance);
+    }
+
+    double currentBalance() {
+        return balance;
+    }
+}
+
+public class BankAccount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter initial balance");
+        double initialBalance = sc.nextDouble();
+        Account account = new Account(initialBalance);
+
+        try {
+            System.out.println("Enter withdrawal amount");
+            double withdrawAmount = sc.nextDouble();
+            account.withdraw(withdrawAmount);
+        } catch (InsufficientFundException e) {
+            // System.out.println(e);
+            System.out.println("Error : " + e.getMessage());
+            System.out.println("Current balance : " + account.currentBalance());
+        }
+
+        System.out.println("Deposit some amount");
+        double depositBalance = sc.nextDouble();
+        account.deposit(depositBalance);
+
+        try {
+            System.out.print("Enter withdrawal amount: ");
+            double withdrawAmount = sc.nextDouble();
+            account.withdraw(withdrawAmount);
+        } catch (InsufficientFundException e) {
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("Current balance : " + account.currentBalance());
+        }
+        sc.close();
+    }
+}
