6097941c3f598298c59ece9c541b638f20c13083
ishitakhokhar <144801034+ishitakhokhar@users.noreply.github.com>
2024-05-02 16:23:49 +0530

    Update Range.java

--- Practical -09 Advanced Programs using Exception handling/Range.java
+++ Practical -09 Advanced Programs using Exception handling/Range.java
@@ -1,3 +1,5 @@
+// Write a program in java if number is less than 10 and greater than 50 it generate the
+// exception out of range. Else it displays the square of number.
 import java.util.Scanner;
 
 class OutOfRangeException extends Exception {
