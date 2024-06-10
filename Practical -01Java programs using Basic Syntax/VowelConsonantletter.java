71cce3a9c26ba9734632cecd6aea92f7bdd71c78
ishitakhokhar <144801034+ishitakhokhar@users.noreply.github.com>
2024-06-10 20:50:38 +0530

    Update VowelConsonantletter.java

--- Practical -01Java programs using Basic Syntax/VowelConsonantletter.java
+++ Practical -01Java programs using Basic Syntax/VowelConsonantletter.java
@@ -3,19 +3,19 @@
 //or constant.
 import java.util.Scanner;
 
-public class VowelConsonantletter {
+public class VowelCons {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter letter");
-        String letter = sc.next();
-        if (letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u" || letter == "A"
-                || letter == "E" || letter == "I" || letter == "O"
-                || letter == "U") {
-            System.out.println("the letter is vowel");
-        } else {
-            System.out.println("the letter is consonant");
-        }
-        sc.close();
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Letter : ");
+        String letter = scanner.next();
 
+        if(letter.equals("a") || letter.equals("e") || letter.equals("i") || letter.equals("o") || letter.equals("u") ||
+           letter.equals("A") || letter.equals("E") || letter.equals("I") || letter.equals("O") || letter.equals("U")) {
+            System.out.println("Letter is vowel");
+        } else {
+            System.out.println("Letter is consonant");
+        }
+
+        scanner.close();
+    }
 }
